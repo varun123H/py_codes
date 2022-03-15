d400e2b3f91bcc7fd0da57abeea01e43c97cbc5a
Varun Badave <varunbadave99@gmail.com>
2022-03-15 23:48:16 +0530

    fizzbuzz and crowd comp

---
+++ week2/2_crowd_comp.py
@@ -0,0 +1,9 @@
+
+from statistics import mean
+
+
+sample= [10,45,67,98,43,78,45,76,32,54,67,90,34,12,56,45,88,55,88]
+sample.sort()
+sampled = sample[2:17]
+print(mean(sampled))
+
--- week2/main.py
+++
@@ -1,4 +0,0 @@
-ages= [4,6,3,8,2,9]
-ages.sort()
-for i in range(len(ages)):
-    print(ages[i])
